3a02aef90e795f8d8f0eeb82a50ba18a1370e4cb
WendelSReis <wendel.m.w18@gmail.com>
2023-10-04 11:55:54 -0300

    Add App

--- src/main/java/org/cadastros/App.java
+++ src/main/java/org/cadastros/App.java
@@ -1,4 +1,19 @@
 package org.cadastros;
 
+import org.cadastros.dao.ClienteMapDAO;
+import org.cadastros.dao.IClienteDAO;
+
+import javax.swing.*;
+
 public class App {
+
+    private static IClienteDAO iClienteDAO;
+    public static  void main(String args[]) {
+        iClienteDAO = new ClienteMapDAO();
+
+        String opcao = JOptionPane.showInputDialog(null,
+                "Digite 1 para cadastro, 2 para consultar, 3 para exclusão, 4 para alteração ou 5 para sair",
+                "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+
+    }
 }
