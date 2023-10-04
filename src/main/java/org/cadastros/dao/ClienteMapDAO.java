82d3c018527bee0a25263dabb6cacefa2d0f5fc4
WendelSReis <wendel.m.w18@gmail.com>
2023-10-04 10:07:57 -0300

    Adicionando metodos

--- src/main/java/org/cadastros/dao/ClienteMapDAO.java
+++ src/main/java/org/cadastros/dao/ClienteMapDAO.java
@@ -25,21 +25,32 @@ public class ClienteMapDAO implements IClienteDAO {
 
     @Override
     public void excluir(Long cpf) {
+        Cliente clienteCadastrado = this.map.get(cpf);
 
+        if (clienteCadastrado != null) {
+            this.map.remove(clienteCadastrado.getCpf(), clienteCadastrado);
+        }
     }
 
     @Override
     public void alterar(Cliente cliente) {
-
+        Cliente clienteCadastrado = this.map.get(cliente.getCpf());
+        if (clienteCadastrado != null) {
+            clienteCadastrado.setNome(cliente.getNome());
+            clienteCadastrado.setTel(cliente.getTel());
+            clienteCadastrado.setNumero(cliente.getNumero());
+            clienteCadastrado.setCidade(cliente.getCidade());
+            clienteCadastrado.setEstado(cliente.getEstado());
+        }
     }
 
     @Override
     public Cliente consultar(Long cpf) {
-        return null;
+        return this.map.get(cpf);
     }
 
     @Override
     public Collection<Cliente> buscarTodos() {
-        return null;
+        return this.map.values();
     }
 }
